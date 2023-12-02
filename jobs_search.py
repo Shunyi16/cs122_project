42c7bbf1849aa1bf4191afa0c9341c8b37e18856
TamNganLy <98865980+TamNganLy@users.noreply.github.com>
2023-12-02 00:57:58 -0800

    fixed errors

--- jobs_search.py
+++ jobs_search.py
@@ -3,11 +3,12 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-file = open('linkedin-jobs.csv', 'w')
-writer = csv.writer(file, lineterminator='\n')
-writer.writerow(['Title', 'Time', 'Company', 'Location', 'Link'])
 
-def linkedin_scraper(webpage, page_number):
+def linkedin_scraper(webpage, page_number, writer):
+    """
+    Get data from Linked In and save into a csv file
+    """
+
     next_page = webpage + str(page_number)
     print(str(next_page))
     response = requests.get(str(next_page))
@@ -16,6 +17,7 @@ def linkedin_scraper(webpage, page_number):
     jobs = soup.find_all('div', {'class':'base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card'})
     for job in jobs:
         job_title = job.find('h3', {'class':'base-search-card__title'}).text.strip()
+        job_title = job_title.replace(",", ".")
         job_company = job.find('h4', class_='base-search-card__subtitle').text.strip()
         job_location = job.find('span', class_='job-search-card__location').text.strip()
         job_link = job.find('a', class_='base-card__full-link')['href']
@@ -28,16 +30,18 @@ def linkedin_scraper(webpage, page_number):
 
     if page_number < 25:
         page_number = page_number + 25
-        linkedin_scraper(webpage, page_number)
-    else:
-        file.close()
-        print('File closed')
-    
+        linkedin_scraper(webpage, page_number, writer)   
 
 def new_search(input_position):
+    """
+    Take the position from input and pass the linkedin_scraper
+    """
     position = '%20'.join(input_position.split())
     print(position)
     url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=' + position + '&location=United%20States&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start='
-    linkedin_scraper(url, 0)
+    with open('linkedin-jobs.csv', "w", encoding="utf-8") as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow(['Title', 'Time', 'Company', 'Location', 'Link'])
+        linkedin_scraper(url, 0, writer)
 
 
